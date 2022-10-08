24e7ddd062dee7faa92266f8fb28e4b95ae159f7
victor <victor.robic@efrei.net>
2022-10-07 19:06:54 -0700

    Modification script

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,8 @@
 async function callapi(){
     let type = document.getElementById("selecttype").value;
     let number = document.getElementById("number").value
-    let response = await fetch('http://numbersapi.com/'+number+'/'+type);
-    let data = await response.text();
+    let response = await fetch('http://numbersapi.com/'+number+'/'+type+'?json');
+    let data = await response.json();
     document.getElementById("display").className = 'alert alert-primary'
-    document.getElementById("display").innerHTML =  data;
+    document.getElementById("display").innerHTML =  data.text;
 }
